f75c82936063832f38382e174231e25d32e0b0fd
Inge Becht <inge.becht91@gmail.com>
2013-10-04 23:51:17 +0200

    added morphfit and distance calculator as new functions

--- exe/mmr_lib.py
+++ exe/mmr_lib.py
@@ -4,6 +4,7 @@
 
 import os
 import shutil
+import math
 
 ###  Contents
     ## Big functions
@@ -27,6 +28,7 @@ import shutil
         # Example evaluation functions
 
 ### Constants
+n = 30
 mmr_path = "..\\"
 exe_path = mmr_path + "exe\\"
 scan_path = mmr_path + "scans\\"
@@ -38,6 +40,9 @@ test_path = mmr_path + "tests\\"
 # Below are the folders within each test folder
 facecor_folder = "FaceCorrespondence\\"
 mm_folder = "MorphableModel\\"
+query_folder = "QuerySet\\"
+test_folder = "TestSet\\"
+result_folder = "Results\\"
 
 ###
 
@@ -49,7 +54,7 @@ mm_folder = "MorphableModel\\"
 # Uses the constants, makes a MM out of the first 30 faces.
 def build_mm_fast (test_name):
     # TODO: remove 'first 30 faces' hardcoding
-    training_set = range(477, 477 + 30)
+    training_set = range(477, 477 + n)
     facecor_path = test_path + test_name + "\\" + facecor_folder
     
     # Check if the path exists
@@ -79,6 +84,48 @@ def build_mm_fast (test_name):
     os.system(command)
 
 # 2. Query the scans
+# Applies morphfit using the existing data.bin file on both the query set and the test set
+# to find the final params and writes these to the right directory
+def morphfit_scans (test_name):
+
+	# Path to query and test directories
+	query_path = test_path + test_name + "\\" + query_folder
+	testset_path = test_path + test_name + "\\" + test_folder
+	
+	# Path to the morphable model
+	mm_path = test_path + test_name + "\\" + mm_folder + "data.bin"
+	
+	principal_components = str(n-1)
+	
+	# Apply morphfit to the  query set
+	command = "morphfit " + query_path + " 1 " + mm_path + " " + principal_components
+	print "Executing", command
+	os.system(command)
+	
+	# Move *_final.params to Query directory
+	move_files_filter(".\\", query_path, ["final.params"])
+	
+	# Apply morphfit to the  test set
+	command = "morphfit " + testset_path + " 1 " + mm_path + " " + principal_components
+	print "Executing", command
+	os.system(command)
+	
+	# Move *_final.params to Test directory
+	move_files_filter(".\\", testset_path, ["final.params"])
+	
+	#Path to results. If it not yet exists, make it
+	result_path = test_path + test_name + "\\" + result_folder
+	if not os.path.exists(result_path):
+		os.makedirs(result_path)
+	
+	#loop through all files in query path
+	for filename in os.listdir(query_path):
+		# make sure the file is a .params
+		if not ("final.params" in filename) :
+			continue
+		f = open(query_path + filename, 'r')
+		print 'Calculating distance to test set for file: ' + filename 
+		evaluate_distances(f, testset_path, result_path)
 
 # 3. Evaluate query results
 
@@ -102,15 +149,77 @@ def copy_files_filter (from_dir, to_dir, filter_list):
             if copied: break
             if str(name_filter) in filename and not copied:
                 #print filename # <- optional spam print
-                shutil.copy(from_dir + filename, to_dir)
-                copied = True
-            
+				shutil.copy(from_dir + filename, to_dir)
+				copied = True
+				
+ # Move files #
+ # Move files that have the substring filter_list in them from from_dir to to_dir
+def move_files_filter (from_dir, to_dir, filter_list):
+    print "Moving from", from_dir, "to", to_dir
+    print "  with name filters:"
+    print filter_list
+    
+    for filename in os.listdir(from_dir):
+        for name_filter in filter_list:
+            if str(name_filter) in filename:
+                #print filename # <- optional spam print
+                shutil.move(from_dir + filename, to_dir)
+				
+  # Evaluate distances #
+ # Evaluates the distance between a single query files and all test files
+def evaluate_distances(q_file, test_path, result_path):
+	q_vector = file_to_list(q_file)
+	name = q_file.name[-27:-24]
+
+	# create file containing ordered list of file rankings
+	result_file = open(result_path + name + '.txt', 'w+')
+	
+	#list of distances
+	distances = []
+	
+	for filename in os.listdir(test_path):
+		# make sure the file is a .params
+		if not ("final.params" in filename) :
+			continue
+		t_file = open(test_path + filename, 'r')
+		t_vector = file_to_list(t_file)
+		dist = calc_distance(q_vector, t_vector)
+		distances = distances + [(filename, dist)]
+	# Sort the distances
+	distances = sorted(distances,key=lambda x: x[1])
+	for candidate in distances:
+		result_file.write("%s: " % candidate[0])
+		result_file.write("%f\n" % candidate[1])
+	result_file.close()
+ 
+ 
 # Fit mm
-# Calculate distance
+# Calculate distance #
+# Returns the Euclidian distance between 2 lists (interpreted as vectors)
+def calc_distance(q_vec, s_vec):
+	cumulative = 0
+	for i in range(0, len(q_vec)):
+		x = q_vec[i]
+		y = s_vec[i]
+		cumulative = cumulative + (x - y)**2
+	return math.sqrt(cumulative)
+
+# File to list #
+# read in a file descriptor and return a list
+def file_to_list(f):
+	#loop through all
+	values = f.readline()
+	p = values.split('\t')
+	# Remove the newline
+	p = p[:-1]
+	p = map(float, p)
+	return p	
+	
 # Rank scans
 
 ## Example plug-in functions
 # Example distance functions
 # Example evaluation functions
 
-build_mm_fast("test1")
+#build_mm_fast("test1")
+morphfit_scans("test1")
